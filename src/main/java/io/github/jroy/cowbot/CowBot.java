09fb59d950d3bffe012891b6f826854905cbf133
JRoy <joshroy126@gmail.com>
2019-03-23 22:37:10 -0400

    Add sleep monitor

--- src/main/java/io/github/jroy/cowbot/CowBot.java
+++ src/main/java/io/github/jroy/cowbot/CowBot.java
@@ -9,13 +9,19 @@ import net.dv8tion.jda.core.JDA;
 import net.dv8tion.jda.core.JDABuilder;
 import net.dv8tion.jda.core.OnlineStatus;
 import org.bukkit.Bukkit;
+import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
+import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerBedEnterEvent;
+import org.bukkit.event.player.PlayerBedLeaveEvent;
 import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.plugin.java.JavaPlugin;
 
 import javax.security.auth.login.LoginException;
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Objects;
 
 public class CowBot extends JavaPlugin implements Listener {
@@ -24,6 +30,8 @@ public class CowBot extends JavaPlugin implements Listener {
 
   private JDA jda;
   public DatabaseFactory databaseFactory;
+
+  private List<Player> sleeping = new ArrayList<>();
 
   @Override
   public void onEnable() {
@@ -59,7 +67,25 @@ public class CowBot extends JavaPlugin implements Listener {
     }
   }
 
-  @EventHandler
+  @EventHandler(priority = EventPriority.HIGH)
+  public void onSleep(PlayerBedEnterEvent event) {
+    if (event.getBedEnterResult() == PlayerBedEnterEvent.BedEnterResult.OK) {
+      sleeping.add(event.getPlayer());
+      if (sleeping.size() >= (event.getPlayer().getWorld().getPlayers().size() / 2)) {
+        Bukkit.getServer().getWorld(event.getPlayer().getWorld().getName()).setTime(1000L);
+        event.getPlayer().getWorld().setStorm(false);
+        event.getPlayer().getWorld().setThundering(false);
+        sleeping.clear();
+      }
+    }
+  }
+
+  @EventHandler(priority = EventPriority.HIGH)
+  public void onUnSleep(PlayerBedLeaveEvent event) {
+    sleeping.remove(event.getPlayer());
+  }
+
+  @EventHandler(priority = EventPriority.HIGHEST)
   public void onJoin(PlayerJoinEvent event) {
     if (!databaseFactory.isWhitelisted(event.getPlayer().getName())) {
       event.setJoinMessage("");
