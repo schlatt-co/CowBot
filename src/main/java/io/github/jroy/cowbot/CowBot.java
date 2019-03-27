6e195bac9329d30ea66c208392c0cbf01f6283f9
JRoy <joshroy126@gmail.com>
2019-03-26 21:24:01 -0400

    Add sleeping broadcasts

--- src/main/java/io/github/jroy/cowbot/CowBot.java
+++ src/main/java/io/github/jroy/cowbot/CowBot.java
@@ -9,6 +9,7 @@ import net.dv8tion.jda.core.JDA;
 import net.dv8tion.jda.core.JDABuilder;
 import net.dv8tion.jda.core.OnlineStatus;
 import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
@@ -16,6 +17,7 @@ import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerBedEnterEvent;
 import org.bukkit.event.player.PlayerBedLeaveEvent;
 import org.bukkit.event.player.PlayerJoinEvent;
+import org.bukkit.event.player.PlayerQuitEvent;
 import org.bukkit.plugin.java.JavaPlugin;
 
 import javax.security.auth.login.LoginException;
@@ -72,10 +74,13 @@ public class CowBot extends JavaPlugin implements Listener {
     if (event.getBedEnterResult() == PlayerBedEnterEvent.BedEnterResult.OK) {
       sleeping.add(event.getPlayer());
       if (sleeping.size() >= (event.getPlayer().getWorld().getPlayers().size() / 2)) {
+        Bukkit.broadcastMessage(ChatColor.AQUA + "[Trevor from Cowchop] " + ChatColor.WHITE + "Advancing to day!");
         Bukkit.getServer().getWorld(event.getPlayer().getWorld().getName()).setTime(1000L);
         event.getPlayer().getWorld().setStorm(false);
         event.getPlayer().getWorld().setThundering(false);
         sleeping.clear();
+      } else {
+        Bukkit.broadcastMessage(ChatColor.AQUA + "[Trevor from Cowchop] " + ChatColor.YELLOW + event.getPlayer().getName() + ChatColor.WHITE + " has started sleeping! " + ChatColor.YELLOW + ((event.getPlayer().getWorld().getPlayers().size() / 2) - sleeping.size()) + ChatColor.WHITE + " more player(s) need to sleep in order to advance to day!");
       }
     }
   }
