38ecf04745b9280de610ff17effaa276764729ff
JRoy <joshroy126@gmail.com>
2019-04-01 20:36:50 -0400

    Add owner only constructor parameter

--- src/main/java/io/github/jroy/cowbot/commands/base/CommandBase.java
+++ src/main/java/io/github/jroy/cowbot/commands/base/CommandBase.java
@@ -27,9 +27,22 @@ public abstract class CommandBase extends Command {
    * @param helpMessage    Command description to be used inside the command list.
    */
   public CommandBase(@NotNull String commandName, String arguments, String helpMessage) {
+    this(commandName, arguments, helpMessage, false);
+  }
+
+  /**
+   * Constructor for commands with certain role requires for execution.
+   *
+   * @param commandName    Command's name to be used for execution.
+   * @param arguments      Arguments of the command to be used for command help.
+   * @param helpMessage    Command description to be used inside the command list.
+   * @param ownerOnly      Is the command only executable for the owner.
+   */
+  public CommandBase(@NotNull String commandName, String arguments, String helpMessage, boolean ownerOnly) {
     this.name = commandName;
     this.arguments = arguments;
     this.help = helpMessage;
+    this.ownerCommand = ownerOnly;
     this.invalid = "**Correct Usage:**" + " ^" + name + " " + arguments;
   }
 
