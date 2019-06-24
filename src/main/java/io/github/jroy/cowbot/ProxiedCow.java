02217f1401d0f27951720702e3f739811958d103
JRoy <joshroy126@gmail.com>
2019-06-24 14:32:54 -0400

    Prepare for 1.14.3

--- src/main/java/io/github/jroy/cowbot/ProxiedCow.java
+++ src/main/java/io/github/jroy/cowbot/ProxiedCow.java
@@ -43,8 +43,8 @@ public class ProxiedCow extends Plugin implements Listener {
   public boolean isLockdown = false;
   public List<String> lockdownList = new ArrayList<>();
 
-  private String targetVersion = "1.14.2";
-  private int targetProtocol = 485;
+  private String targetVersion = "1.14.3";
+  private int targetProtocol = 490;
   private String targetServer = "vanilla";
 
   public static String serverMotd = "&ajschlatt twitch subscriber server\n&bsubscribe with &5twitch prime&b!";
