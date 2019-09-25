e0e18826a8c717f634abd8b96eef4980d16cc6ff
JRoy <joshroy126@gmail.com>
2019-09-24 20:25:32 -0400

    Make dirt unstackable

--- src/main/java/io/github/jroy/cowbot/commands/spigot/DirtCommand.java
+++ src/main/java/io/github/jroy/cowbot/commands/spigot/DirtCommand.java
@@ -1,5 +1,6 @@
 package io.github.jroy.cowbot.commands.spigot;
 
+import org.bukkit.ChatColor;
 import org.bukkit.Material;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
@@ -8,7 +9,17 @@ import org.bukkit.entity.Player;
 import org.bukkit.inventory.ItemStack;
 import org.jetbrains.annotations.NotNull;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
+
 public class DirtCommand implements CommandExecutor {
+
+  private final ItemStack dirt;
+
+  public DirtCommand() {
+    this.dirt = new ItemStack(Material.DIRT);
+  }
 
   @Override
   public boolean onCommand(@NotNull CommandSender sender, @NotNull Command command, @NotNull String label, @NotNull String[] args) {
@@ -17,8 +28,15 @@ public class DirtCommand implements CommandExecutor {
     }
     Player player = (Player) sender;
 
-    player.getInventory().addItem(new ItemStack(Material.DIRT));
+    newDirt();
+    player.getInventory().addItem(dirt);
     player.sendMessage("dirt");
     return true;
+  }
+
+  private void newDirt() {
+    List<String> lore = new ArrayList<>();
+    lore.add(ChatColor.RED + "Dirt ID; " + UUID.randomUUID().toString());
+    dirt.setLore(lore);
   }
 }
