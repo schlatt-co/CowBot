9db4e680bcb3b735489c6fe9356bf317666ed7d0
JRoy <joshroy126@gmail.com>
2020-01-06 18:13:00 -0500

    Add essentials /back support for /spawn

--- src/main/java/io/github/jroy/cowbot/commands/spigot/SpawnCommand.java
+++ src/main/java/io/github/jroy/cowbot/commands/spigot/SpawnCommand.java
@@ -1,19 +1,23 @@
 package io.github.jroy.cowbot.commands.spigot;
 
+import com.earth2me.essentials.Essentials;
 import io.github.jroy.cowbot.managers.spigot.CommunismManager;
-import io.papermc.lib.PaperLib;
+import org.bukkit.Bukkit;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
+import org.bukkit.event.player.PlayerTeleportEvent;
 import org.jetbrains.annotations.NotNull;
 
 public class SpawnCommand implements CommandExecutor {
 
   private final CommunismManager communismManager;
+  private final Essentials essentials;
 
   public SpawnCommand(CommunismManager communismManager) {
     this.communismManager = communismManager;
+    this.essentials = (Essentials) Bukkit.getServer().getPluginManager().getPlugin("Essentials");
   }
 
   @Override
@@ -24,7 +28,11 @@ public class SpawnCommand implements CommandExecutor {
     Player player = (Player) sender;
 
     assert communismManager.world != null;
-    PaperLib.teleportAsync(player, communismManager.world.getSpawnLocation());
+    try {
+      essentials.getUser(player).getTeleport().now(communismManager.world.getSpawnLocation(), false, PlayerTeleportEvent.TeleportCause.COMMAND);
+    } catch (Exception e) {
+      e.printStackTrace();
+    }
     return true;
   }
 }
