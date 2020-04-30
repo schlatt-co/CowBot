0bf63495db4bcec5fc618a29fae2cf0780ce59be
JRoy <joshroy126@gmail.com>
2020-04-29 23:04:17 -0400

    Improve JoinDateCommand

--- src/main/java/io/github/jroy/cowbot/commands/spigot/JoinDateCommand.java
+++ src/main/java/io/github/jroy/cowbot/commands/spigot/JoinDateCommand.java
@@ -1,6 +1,8 @@
 package io.github.jroy.cowbot.commands.spigot;
 
+import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
+import org.bukkit.OfflinePlayer;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
@@ -22,12 +24,19 @@ public class JoinDateCommand implements CommandExecutor {
       return true;
     }
 
-    Player player = (Player) sender;
+    OfflinePlayer target = (Player) sender;
 
-    OffsetDateTime joinDate = Instant.ofEpochMilli(player.getFirstPlayed()).atOffset(ZoneOffset.UTC);
+    if (args.length == 1 && sender.hasPermission("trevor.mod")) {
+      //noinspection deprecation
+      target = Bukkit.getOfflinePlayer(args[0]);
+      if (!target.hasPlayedBefore()) {
+        sender.sendMessage("User has not played before (or joined today)");
+        return true;
+      }
+    }
 
-    player.sendMessage(ChatColor.YELLOW + "Your join date is " + joinDate.getMonth().getDisplayName(TextStyle.FULL, Locale.US) + " " + joinDate.getDayOfMonth() + " " + joinDate.getYear());
-
+    OffsetDateTime joinDate = Instant.ofEpochMilli(target.getFirstPlayed()).atOffset(ZoneOffset.UTC);
+    sender.sendMessage(ChatColor.YELLOW + "Your join date is " + joinDate.getMonth().getDisplayName(TextStyle.FULL, Locale.US) + " " + joinDate.getDayOfMonth() + " " + joinDate.getYear());
     return true;
   }
 }
